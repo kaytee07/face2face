37fa1703385f90c54a409b7a5947e7fe46e2c089
kaytee07 <kofi.taylor07@gmail.com>
2024-04-09 13:46:18 +0000

    navbar logo created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='flex-between x-50 w-full bg-dark-1
+     px-6 py-1 lg:px-10'>
+      <Link
+        href="/"
+        className='flex items-center gap-1'
+      >
+        <Image
+           src="/icons/logo.svg"
+           alt='F2F logo'
+           height={32}
+           width={32}
+           className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'
+        >Face2Face</p>
+      </Link>
+    </nav>
   )
 }
 
