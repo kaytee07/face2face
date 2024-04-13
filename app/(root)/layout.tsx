4798a2ff0fa84bf2460341a58fd44cfa60991373
kaytee07 <kofi.taylor07@gmail.com>
2024-04-13 13:07:52 +0000

    setting up streamvideo configurations

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
+
+import StreamVideoProvider from '@/providers/streamClientProvider';
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <div>
-      {children}
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
     </div>
   )
 }
